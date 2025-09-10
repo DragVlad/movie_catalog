3fb0909ae602cfe13d65f942362c8508a9843205
Vladislav Dragushin <maxhc@inbox.ru>
2025-09-10 20:55:36 +0300

    Added MovieCreate to POST scheme

--- movie_api/schemas/movies.py
+++ movie_api/schemas/movies.py
@@ -1,12 +1,34 @@
+from typing import (
+    Annotated,
+    Optional,
+)
+from annotated_types import Len
 from pydantic import BaseModel
 
 
 class MovieBase(BaseModel):
     slug: str
     name: str
-    description: str
-    author: str
+    description: Optional[str] = None
+    author: Optional[str] = None
 
 
 class Movie(MovieBase):
     pass
+
+
+class MovieCreate(Movie):
+    slug: Annotated[
+        str,
+        Len(
+            min_length=3,
+            max_length=10,
+        ),
+    ]
+    name: Annotated[
+        str,
+        Len(
+            min_length=5,
+            max_length=20,
+        ),
+    ]
