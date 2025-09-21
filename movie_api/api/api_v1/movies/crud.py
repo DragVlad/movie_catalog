9379fc7824314f4ee311d872100f4122b3150600
Vladislav Dragushin <maxhc@inbox.ru>
2025-09-21 17:21:00 +0300

    Update crud to file

--- movie_api/api/api_v1/movies/crud.py
+++ movie_api/api/api_v1/movies/crud.py
@@ -4,12 +4,25 @@ from schemas.movies import (
     MoviePartialUpdate,
     MovieUpdate,
 )
+from core.config import MOVIE_STORAGE_FILEPATH
 
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel,
+    ValidationError,
+)
 
 
 class MovieStorage(BaseModel):
     slug_to_movie: dict[str, Movie] = {}
+
+    def save_state(self) -> None:
+        MOVIE_STORAGE_FILEPATH.write_text(self.model_dump_json(indent=2))
+
+    @classmethod
+    def from_state(cls) -> "MovieStorage":
+        if not MOVIE_STORAGE_FILEPATH.exists():
+            return MovieStorage()
+        return cls.model_validate_json(MOVIE_STORAGE_FILEPATH.read_text())
 
     def get_movies(self) -> list[Movie]:
         return list(self.slug_to_movie.values())
@@ -22,10 +35,12 @@ class MovieStorage(BaseModel):
             **movie_in.model_dump(),
         )
         self.slug_to_movie[movie.slug] = movie
+        self.save_state()
         return movie
 
     def delete_by_slug(self, slug: str) -> None:
         self.slug_to_movie.pop(slug, None)
+        self.save_state()
 
     def delete_movie(self, movie: Movie) -> None:
         self.delete_by_slug(slug=movie.slug)
@@ -37,7 +52,7 @@ class MovieStorage(BaseModel):
     ):
         for field_name, value in movie_in:
             setattr(movie, field_name, value)
-
+        self.save_state()
         return movie
 
     def update_partial_movie(
@@ -47,41 +62,12 @@ class MovieStorage(BaseModel):
     ):
         for field_name, value in movie_in.model_dump(exclude_unset=True).items():
             setattr(movie, field_name, value)
-
+        self.save_state()
         return movie
 
 
-storage = MovieStorage()
-
-storage.create_movie(
-    Movie(
-        slug="stlr",
-        name="Интерстеллар",
-        description="Фантастический фильм о путешествии через червоточину в поисках нового дома для человечества",
-        author="Кристофер Нолан",
-    ),
-)
-storage.create_movie(
-    Movie(
-        slug="fthr",
-        name="Крестный отец",
-        description="Эпическая сага о сицилийской мафиозной семье Корлеоне",
-        author="Фрэнсис Форд Коппола",
-    ),
-)
-storage.create_movie(
-    Movie(
-        slug="lrdrng",
-        name="Властелин колец: Братство кольца",
-        description="Фэнтези о путешествии хоббита Фродо по уничтожению Кольца Всевластия",
-        author="Питер Джексон",
-    ),
-)
-storage.create_movie(
-    Movie(
-        slug="gmp",
-        name="Форрест Гамп",
-        description="История жизни простого человека, ставшего свидетелем ключевых событий американской истории",
-        author="Роберт Земекис",
-    ),
-)
+try:
+    storage = MovieStorage.from_state()
+except ValidationError:
+    storage = MovieStorage()
+    storage.save_state()
