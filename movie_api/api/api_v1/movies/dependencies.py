139c7da3b3b7eecf7ef31aa98cc22c6b43eb5a1a
Vladislav Dragushin <maxhc@inbox.ru>
2025-09-25 22:32:19 +0300

    Added background func save_storage_state

--- movie_api/api/api_v1/movies/dependencies.py
+++ movie_api/api/api_v1/movies/dependencies.py
@@ -1,10 +1,16 @@
+import logging
+
 from .crud import storage
 from schemas.movies import Movie
 
 from fastapi import (
     HTTPException,
+    BackgroundTasks,
     status,
 )
+
+
+log = logging.getLogger(__name__)
 
 
 def found_movie(
@@ -18,3 +24,11 @@ def found_movie(
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"Movie {slug!r} not found",
     )
+
+
+def save_storage_state(
+    background_tasks: BackgroundTasks,
+):
+    yield
+    log.info("Add background task to save movie storage")
+    background_tasks.add_task(storage.save_state)
